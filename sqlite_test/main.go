10d55134e8adf300cc89f3f0a41e70f48b610636
drakeeeleee <drakeeeleee@gmail.com>
2025-04-19 10:58:21 +0900

    Implement main function

--- sqlite_test/main.go
+++ sqlite_test/main.go
@@ -1,19 +1,45 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"sqlitetest/mydb"
 	"sqlitetest/mydb/model"
+	"sync"
 )
 
 func main() {
-	dbName := "test.db"
+	dbName := "main.db"
 	db, err := mydb.NewLocalDb(dbName)
-	defer func() { _ = os.Remove(dbName) }()
 	if err != nil {
 		log.Fatalf("failed to make local db: %v", err)
 	}
+	defer func() { _ = os.Remove(dbName) }()
+	taskDao := mydb.NewTaskDao(db)
 
-	db.Create(&model.Task{TaskId: "task_id_1"})
+	tasks := []*model.Task{
+		{TaskId: "task_id_1"},
+		{TaskId: "task_id_2"},
+		{TaskId: "task_id_3"},
+	}
+
+	numGoroutine := 10
+	var wait sync.WaitGroup
+	wait.Add(numGoroutine)
+	for i := range numGoroutine {
+		go func() {
+			tasks := tasks
+			if existence, err := taskDao.CheckAllExistence(tasks); err != nil {
+				log.Fatalf("failed to check existence at %dth tria: %v", i, err)
+			} else if existence {
+				fmt.Printf("tasks already exist at %dth trial\n", i)
+			} else if err = taskDao.Create(tasks); err != nil {
+				log.Fatalf("failed to create tasks at %dth tria: %v", i, err)
+			}
+			wait.Done()
+		}()
+	}
+
+	wait.Wait()
 }
